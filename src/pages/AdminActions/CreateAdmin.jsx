d4efad27e67a6794ab5f4e371d1c60d77b68cbea
Hager Tamer <hagertamer16@hotmail.com>
2026-03-12 17:53:33 +0200

    structure: initial pages setup

---
+++ src/pages/AdminActions/CreateAdmin.jsx
@@ -0,0 +1,7 @@
+export default function CreateAdmin() {
+  return (
+    <div>
+      <h1>Create Admin Page</h1>
+    </div>
+  );
+}
---
+++ src/pages/Managements/Requests.jsx
@@ -0,0 +1,7 @@
+export default function Requests() {
+  return (
+    <div>
+      <h1>Requests Page</h1>
+    </div>
+  );
+}
